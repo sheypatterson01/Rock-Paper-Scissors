d0474697a9fbe6598cebb6c0af5f1b64ed7a61df
Shey Patterson <106495685+sheypatterson01@users.noreply.github.com>
2023-12-31 11:40:11 -0600

    Add files via upload
    completed rock paper scissors game with score counter

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const choices = ["rock", "paper", "scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice){
 
@@ -36,9 +40,13 @@ function playGame(playerChoice){
     switch(result){
         case "YOU WIN!":
             resultDisplay.classList.add("winText");
+            playerScore++;
+            playerScoreDisplay.textContent = playerScore;
             break;
         case "YOU LOSE":
             resultDisplay.classList.add("loseText");
+            computerScore++;
+            computerScoreDisplay.textContent = computerScore;
             break;
     }
 }
