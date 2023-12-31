c525991ddc2989bb0463b44bf2c34a7931009153
Shey Patterson <106495685+sheypatterson01@users.noreply.github.com>
2023-12-31 11:22:00 -0600

    Add files via upload
    Progress update

---
+++ index.js
@@ -0,0 +1,44 @@
+// RPS Game
+
+const choices = ["rock", "paper", "scissors"];
+const playerDisplay = document.getElementById("playerDisplay");
+const computerDisplay = document.getElementById("computerDisplay");
+const resultDisplay = document.getElementById("resultDisplay");
+
+function playGame(playerChoice){
+
+    const computerChoice = choices[Math.floor(Math.random()*3)];
+    let result = "";
+
+    if(playerChoice === computerChoice){
+        result = "IT'S A TIE!"
+    }
+    else{
+        switch(playerChoice){
+            case "rock":
+                result  =  (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE";
+                break;
+            case "paper":
+                result  =  (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE";
+                break;
+            case "scissors":
+                result  =  (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE";
+                break;
+        }
+    }
+
+    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+    computerDisplay.textContent = `COMPUTER: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+    resultDisplay.classList.remove("winText", "loseText");
+
+    switch(result){
+        case "YOU WIN!":
+            resultDisplay.classList.add("winText");
+            break;
+        case "YOU LOSE":
+            resultDisplay.classList.add("loseText");
+            break;
+    }
+}
